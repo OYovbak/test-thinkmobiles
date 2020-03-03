b66a8d3a561aaaa98ddd5cd9fd00675cd0b6d737
OYovbak <o.jovbak.03.07@gmail.com>
2020-03-03 07:33:10 +0200

    profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
-    public function show($id)
+    public function show()
     {
-        $user = User::findOrFail($id)->first();
+        $user = User::findOrFail(Auth::user()->id);
 
         if ($user) {
             return view('profile')->withUser($user);
@@ -20,26 +22,45 @@ class ProfileController extends Controller
         }
     }
 
-    public function updateUser($id, Request $request)
+    public function updateUser(Request $request)
     {
-        $user = User::find($id);
-
-        if ($request->name) {
+        $user = User::find(Auth::user()->id);
+        $validator = Validator::make($request->all(), [
+            'name' => ['max:255'],
+            'phone_number' =>['nullable','min:10', 'max:10'],
+            'email' => ['nullable', 'email', 'max:255', 'unique:users'],
+            'password' => ['nullable', 'min:8', 'confirmed'],
+        ]);
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+        if ($request->name && $request->name != $user->name) {
             $user->name = $request->name;
         }
         if ($request->email) {
             $user->email = $request->email;
         }
-        if ($request->phone_number) {
+        if ($request->phone_number &&  $request->phone_number != $user->phone_number) {
             $user->phone_number = $request->phone_number;
         }
         if ($request->password) {
             $user->password = Hash::make($request->password);
         }
+        if ($request->birthday &&  $request->birthday != $user->birthday) {
+        $user->birthday = $request->birthday;
+        }
         $user->save();
-
         return back();
+    }
 
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['string', 'max:255'],
+            'phone_number' =>['string', 'min:10', 'max:10'],
+            'email' => ['string', 'email', 'max:255', 'unique:users'],
+            'password' => ['string', 'min:8', 'confirmed'],
+        ]);
     }
 
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,50 +1,125 @@
 @extends('layouts.app')
-<style>
-    button {
-        display: inline-block;
-        font-family: arial, sans-serif;
-        font-size: 11px;
-        color: rgb(205, 216, 228);
-        text-shadow: 0 -1px rgb(46, 53, 58);
-        text-decoration: none;
-        user-select: none;
-        line-height: 2em;
-        padding: 1px 1.2em;
-        outline: none;
-        border: 1px solid rgba(33, 43, 52, 1);
-        border-radius: 3px;
-        background: rgb(81, 92, 102) linear-gradient(rgb(81, 92, 102), rgb(69, 78, 87));
-        box-shadow: inset 0 1px rgba(101, 114, 126, 1),
-        inset 0 0 1px rgba(140, 150, 170, .8),
-        0 1px rgb(83, 94, 104),
-        0 0 1px rgb(86, 96, 106);
-    }
-</style>
 @section('content')
+    <style>
+            #email.badge.badge-light {
+            display: block;
+            padding: 0.375rem 0.75rem;
+            font-weight: 400;
+            line-height: 1.6;
+            text-align: left;
+            white-space: nowrap;
+            border-radius: 0.30rem;
+            transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+            height: calc(1.6em + 0.75rem + 2px);
+            font-size: 0.9rem;
+        }
+    </style>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Profile</div>
+
+                    <div class="card-body">
                     <form action="/user-update/{{ $user->id }}"
                           method="POST">
                         @csrf
-                        <div style="margin: 10px"><a>Change Name</a>
-                            <div><input name="name"></div>
-                        </div>
-                        <div style="margin: 10px"><a>Change Email</a>
-                            <div><input name="email"></div>
-                        </div>
-                        <div style="margin: 10px"><a>Change Phone</a>
-                            <div><input name="phone_number"></div>
-                        </div>
-                        <div style="margin: 10px"><a>Change Password</a>
-                            <div><input name="password" type="password"></div>
-                        </div>
-                        <button style="width: 100px; height: 30px; margin: 10px" type="submit">submit</button>
-                    </form>
 
-                    <div class="card-body">
+                        <div class="form-group row" style="margin-bottom: 0.5rem">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-mail:</label>
+
+                            <div class="col-md-6">
+                                <label id="email" class="badge badge-light" style="padding: 0.375rem 0.75rem; text-align: left; display: block; font-weight: 400;
+                                font-size: 0.9rem; line-height: 1.6; height: calc(1.6em + 0.75rem + 2px);">{{$user->email}}</label>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" value="{{$user->name}}" name="name" >
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="phone_number" class="col-md-4 col-form-label text-md-right">Phone number</label>
+
+                            <div class="col-md-6">
+                                <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror" value="{{$user->phone_number}}" name="phone_number">
+
+                                @error('phone_number')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="birthday" class="col-md-4 col-form-label text-md-right">Birthday</label>
+
+                            <div class="col-md-6">
+                                <input id="birthday" type="date" class="form-control @error('birthday') is-invalid @enderror" value="{{$user->birthday}}" name="birthday">
+
+                                @error('birthday')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">New E-mail</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email">
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">New password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                        <button class="btn-dark badge-dark" type="submit">Submit</button>
+                            </div>
+                        </div>
+                    </form>
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
